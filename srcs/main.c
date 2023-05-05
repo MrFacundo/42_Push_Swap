810e2c67af257cf955e71193751ef00077704747
Facundo <facundo.troitero@fedrax.com>
2023-05-05 17:35:56 +0100

    utente implementation

--- srcs/main.c
+++ srcs/main.c
@@ -10,36 +10,62 @@ t_stack_node	*get_last_node(t_stack_node *head)
 	return (head);
 }
 
+t_stack_node *get_highest_value_node(t_stack_node *stack)
+{
+	t_stack_node	*node;
+	t_stack_node	*highest_value_node;
+
+	node = stack;
+	highest_value_node = node;
+	while (node)
+	{
+		if (node->value > highest_value_node->value)
+			highest_value_node = node;
+		node = node->next;
+	}
+	return (highest_value_node);
+}
+
 static	void push(t_stack_node **dst, t_stack_node **src)
 {
-	t_stack_node	*last_node_one;
-	t_stack_node	*last_node_two;
+	t_stack_node	*node_to_push;
 
 	if (!*src)
 		return ;
-	last_node_one = get_last_node(*dst);
-	last_node_two = get_last_node(*src);
-	last_node_one->next = last_node_two;
-	last_node_two->prev->next = 0;
-	last_node_two->prev = last_node_one;
-	last_node_two->next = 0;
+	node_to_push = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = 0;
+	if (!*dst)
+	{
+		*dst = node_to_push;
+		node_to_push->next = 0;
+	}
+	else
+	{
+		node_to_push->next = *dst;
+		(*dst)->prev = node_to_push;
+		*dst = node_to_push;
+	}
 	printf("push\n");
 }
 
-static	void swap(t_stack_node **stack)
+static void	swap(t_stack_node **head)
 {
-	t_stack_node	*last_node;
-	if (NULL == stack || NULL == *stack)
+	if (!*head || !head)
 		return ;
-	last_node = get_last_node(*stack);
-	last_node->prev = last_node->prev->prev;
-	last_node->next = last_node->prev->next;
-	last_node->prev->next = last_node;
-	last_node->next->prev = last_node;
-	last_node->next->next = 0;
-	printf("swap\n");
+    // Update the head pointer to point to the second node in the list
+	*head = (*head)->next;
+    // Update the previous and next pointers of the former head node
+	(*head)->prev->prev = *head;
+	(*head)->prev->next = (*head)->next;
+    // Update the prev pointer of the last (3rd) node 
+	if ((*head)->next)
+		(*head)->next->prev = (*head)->prev;
+    // Update the new head node 
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
 }
-
 
 static	void rotate(t_stack_node **stack)
 {
@@ -110,6 +136,19 @@ void	stack_init(t_stack_node **stack, char **argv)
 	}
 }
 
+void	sort_three(t_stack_node **stack)
+{
+	t_stack_node	*highest_value_node;
+
+	highest_value_node = get_highest_value_node(*stack);
+	if (*stack == highest_value_node)
+		rotate(stack);
+	else if ((*stack)->next == highest_value_node)
+		r_rotate(stack);
+	if ((*stack)->value > (*stack)->next->value)
+		swap(stack);
+}
+
 void	print_node(t_stack_node *stack)
 {
 	printf("Node address\t value\t prev\t prev address\t next\t next address\n");
@@ -129,27 +168,58 @@ void	print_node(t_stack_node *stack)
 	printf("*********************************\n");
 }
 
+int	stack_size(t_stack_node *stack)
+{
+	int	count;
+
+	if (!stack)
+		return (0);
+	count = 0;
+	while (stack && ++count)
+		stack = stack->next;
+	return (count);
+}
+
+void	sort(t_stack_node **a, t_stack_node **b)
+{
+	int	stack_length;
+	int	i;
+
+	stack_length = stack_size(*a);
+	while (stack_length-- > 3)
+		push(b, a);
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes(*a, *b);
+		move_nodes(a, b);
+	}
+}
 
 int main()
 {
 	t_stack_node *a;
 	t_stack_node *b;
-	t_stack_node *test_stack;
-	t_stack_node *test_stack_two;
-	char *values[5] = {values[0] = "\0", values[1] = "100", values[2] = "200", values[3] = "300", values[4] = 0};
+	int	stack_length;
+	
+	char *values[7] = {
+		values[0] = "\0",
+		values[1] = "1",
+		values[2] = "2",
+		values[3] = "3",
+		values[4] = "-10",
+		values[5] = "99",
+		values[6] = 0
+		};
 	a = 0;
 	b = 0;
 	stack_init(&a, values + 1);
-	stack_init(&b, values + 1);
-	test_stack = a;
-	print_node(test_stack);
-	r_rotate(&test_stack);
-	print_node(test_stack);
-	swap(&test_stack);
-	print_node(test_stack);
-	test_stack_two = b;
-	push(&test_stack, &test_stack_two);
-	print_node(test_stack);
-	print_node(test_stack_two);
+	stack_length = stack_size(a);
+	if (stack_length == 3)
+		sort_three(&a);
+	else if (stack_length == 2)
+		swap(&a);
+	else
+		sort(&a, &b);
 	return (0);
 }
